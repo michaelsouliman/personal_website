66d4fb997f16ca10ab2e65feadf364074e54b4ad
Michael <michaelsouliman@gmail.com>
2022-06-28 00:07:00 -0700

    finish adding pages and yarn install

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import TopBar from "./components/topbar/TopBar";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 //import Settings from "./pages/settings/Settings";
 //import Home from "./pages/home/Home";
 //import Single from "./pages/single/Single";
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <TopBar />
-      <Login />
+      <Register />
     </>
   );
 }
