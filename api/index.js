77f1acf0c7dce14901ede63d35b1973b6e392a13
Michael <michaelsouliman@gmail.com>
2022-06-28 03:57:57 -0700

    users and posts routes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth")
 
-console.log("hellp");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(process.env.mongo_url).then(console.log("connected to mongo")).catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
     console.log("backend is running");
