265b6a6217e03f5c7ef32e554f97c76ffde279db
Michael <michaelsouliman@gmail.com>
2022-07-04 17:44:37 -0700

    changed static elements (changed some text, adding an about me section later)

--- api/client/src/components/header/Header.jsx
+++ api/client/src/components/header/Header.jsx
@@ -4,8 +4,7 @@ export default function Header() {
   return (
     <div className="header">
         <div className="headerTitles">
-            <span className="headerTitleSm">React & Node</span>
-            <span className="headerTitleLg">Blog</span>    
+            <span className="headerTitleLg">MERN Stack Challenge</span>    
         </div>
         <img className ="headerImg" src="https://images.pexels.com/photos/388415/pexels-photo-388415.jpeg?cs=srgb&dl=pexels-jahoo-clouseau-388415.jpg&fm=jpg" alt=""></img>    
     </div>
